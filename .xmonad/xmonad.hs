c5f21eeb16764e715dba4b858028a13737c9b5bb
Alexander Olzem <olzemal@gmail.com>
2021-06-10 12:33:00 +0200

    some updates

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,8 @@
+import XMonad
+
+main = xmonad $ def
+    { borderWidth        = 2
+    , terminal           = "alacritty"
+    , normalBorderColor  = "#cccccc"
+    , focusedBorderColor = "#cd8b00" }
+
