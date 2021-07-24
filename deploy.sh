a511ee460748ff34b719147e939108fd399ba577
Alexander Olzem <olzemal@gmail.com>
2021-07-24 10:18:41 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if [! -d $HOME/.config ]; then
-    mkdir -p $HOME/.config
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p "$HOME/.config"
 fi
-if [! -d $HOME/.config/alacritty ]; then
-    mkdir -p $HOME/.config/alacritty
+if [ ! -d "$HOME/.config/alacritty" ]; then
+    mkdir -p "$HOME/.config/alacritty"
 fi
 
-cp alacritty.yml	$HOME/.config/alacritty/
-cp .aliases		$HOME/
-cp .bashrc		$HOME/
-cp .spectrwm.conf	$HOME/
-cp starship.toml	$HOME/.config/
-cp .vimrc		$HOME/
-cp .zshrc		$HOME/
+cp alacritty.yml	"$HOME/.config/alacritty/"
+cp .aliases		"$HOME/"
+cp .bashrc		"$HOME/"
+cp .spectrwm.conf	"$HOME/"
+cp starship.toml	"$HOME/.config/"
+cp .vimrc		"$HOME/"
+cp .zshrc		"$HOME/"
 
