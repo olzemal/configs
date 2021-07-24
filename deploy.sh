8541d0872d1aba35d9b7b219d9733693688c1d81
Alexander Olzem <olzemal@gmail.com>
2021-07-24 10:15:38 +0000

    moved everything to root and deploy via script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [! -d $HOME/.config ]; then
+    mkdir -p $HOME/.config
+fi
+if [! -d $HOME/.config/alacritty ]; then
+    mkdir -p $HOME/.config/alacritty
+fi
+
+cp alacritty.yml	$HOME/.config/alacritty/
+cp .aliases		$HOME/
+cp .bashrc		$HOME/
+cp .spectrwm.conf	$HOME/
+cp starship.toml	$HOME/.config/
+cp .vimrc		$HOME/
+cp .zshrc		$HOME/
+
