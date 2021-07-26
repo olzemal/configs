efba980e573a62bc5ae21a89ce2626ff8a5d3214
Alexander Olzem <olzemal@gmail.com>
2021-07-26 12:49:33 +0200

    make diff output more compact

--- diff.sh
+++ diff.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-grep '^cp' deploy.sh | awk '{print $2 " " $3}' | sed "s,~,$HOME," | xargs -n2 diff | wc -l
+files=$(grep '^cp' deploy.sh | awk '{print $2 " " $3}' | sed "s,~,$HOME,")
+
+echo $files | xargs -n2 diff -q
 
