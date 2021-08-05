ff2b7bf905047ee6a0836a9f00009e6ee5dcae57
Alexander Olzem <olzemal@gmail.com>
2021-08-05 17:57:54 +0200

    select which configs you want

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
-
-[ -f "$HOME/.bashrc"             ] && mv "$HOME/.bashrc" "$HOME/.bashrc.old"
-[ -f "$HOME/.vmrc"               ] && mv "$HOME/.vimrc" "$HOME/.vimrc.old"
-
-[ ! -d "$HOME/.config"           ] && mkdir -p "$HOME/.config"
-[ ! -d "$HOME/.config/alacritty" ] && mkdir -p "$HOME/.config/alacritty"
-[ ! -d "$HOME/.config/ranger"    ] && mkdir -p "$HOME/.config/ranger"
-
-echo "Installing CLI Tool Configs"
-ln -s "$PWD/.aliases"           "$HOME/.aliases"
-ln -s "$PWD/starship.toml"	"$HOME/.config/starship.toml"
-ln -s "$PWD/.zshrc"		"$HOME/.zshrc"
-ln -s "$PWD/.vimrc"		"$HOME/.vimrc"
-ln -s "$PWD/.bashrc"		"$HOME/.bashrc"
-ln -s "$PWD/rc.conf"            "$HOME/.config/ranger/rc.conf"
-
-[ "$1" = "cli" ] && exit 0
-
-echo "Installing Desktop Configs"
-ln -s "$PWD/alacritty.yml"      "$HOME/.config/alacritty/alacritty.yml"
-ln -s "$PWD/.spectrwm.conf"	"$HOME/.spectrwm.conf"
+for option in "$@"
+do
+    case $option in
+        alacritty)
+            [ ! -d "$HOME/.config/alacritty" ] && mkdir -p "$HOME/.config/alacritty"
+            ln -s "$PWD/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+            ;;
+        aliases)
+            ln -s "$PWD/shell/.aliases" "$HOME/.aliases"
+            ;;
+        bash)
+            ln -s "$PWD/shell/.bashrc"	"$HOME/.bashrc"
+            ;;
+        ranger)
+            [ ! -d "$HOME/.config/ranger" ] && mkdir -p "$HOME/.config/ranger"
+            ln -s "$PWD/ranger/rc.conf" "$HOME/.config/ranger/rc.conf"
+            ;;
+        spectrwm)
+            ln -s "$PWD/spectrwm/.spectrwm.conf" "$HOME/.spectrwm.conf"
+            ;;
+        starship)
+            ln -s "$PWD/starship/starship.toml" "$HOME/.config/starship.toml"
+            ;;
+	vim)
+            ln -s "$PWD/vim/.vimrc" "$HOME/.vimrc"
+	    ;;
+        zsh)
+            ln -s "$PWD/shell/.zshrc" "$HOME/.zshrc"
+            ;;
+        *)
+            printf "no config file found for \"$option\"\n"
+    esac
+done
 
