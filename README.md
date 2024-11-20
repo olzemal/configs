a2d9e1ff75ccc6bf7e5d1cb51fb9d33e5fe7c1e6
Alexander Olzem <olzemal@pm.me>
2024-11-20 13:06:53 +0100

    Fix flake for home-manager

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # configs
 
 My configuration files and shell scripts.
+
+
+```console
+$ sudo nixos-rebuild switch --flake .#default
+
+$ home-manager switch --flake .
+```
--- flake.nix
+++ flake.nix
@@ -3,24 +3,43 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+  let
+    systemSettings = {
+      system = "x86_64-linux";
+      hostname = "carbon";
+    };
+
+    pkgs = import inputs.nixpkgs {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = (_: true);
+      };
+    };
+  in
+  {
     nixosConfigurations = {
       default = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
         ];
+        specialArgs = { inherit inputs; };
       };
     };
 
     homeConfigurations = {
       "alex" = home-manager.lib.homeManagerConfiguration {
-        specialArgs = { inherit inputs; };
+        inherit pkgs;
         modules = [
           ./home.nix
         ];
+        extraSpecialArgs = { inherit inputs; };
       };
     };
   };
