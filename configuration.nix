6478fe6b6c310aaa2802492864d7733f32282392
Alexander Olzem <olzemal@pm.me>
2024-11-21 08:46:56 +0100

    Enable tailscaled

--- configuration.nix
+++ configuration.nix
@@ -144,6 +144,8 @@
     gdc
   ];
 
+  services.tailscale.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
