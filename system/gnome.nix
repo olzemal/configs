c7228cfb54a126d2d74b35299de39547cbe5d41a
Alexander Olzem <olzemal@pm.me>
2024-12-08 22:18:38 +0100

    Fix error

--- system/gnome.nix
+++ system/gnome.nix
@@ -2,8 +2,8 @@
 
 {
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager = {
-    gdm.enable = true;
-    gnome.enable = true;
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
 }
