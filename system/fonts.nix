e23790700621abaf67441d7eafe531d3ba0afe92
Alexander Olzem <olzemal@pm.me>
2024-12-08 22:34:11 +0100

    Don't download every nerdfont...

--- system/fonts.nix
+++ system/fonts.nix
@@ -2,7 +2,7 @@
 
 {
   fonts = {
-    packages = with pkgs; [ nerdfonts ];
+    packages = with pkgs; [ (nerdfonts.override { fonts = [ "JetBrainsMono" "Iosevka" "Hack" ]; }) ];
     fontDir.enable = true;
   };
 }
