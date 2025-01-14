4b183d8c2448191d5ccec356c9b81ff7cde89a0e
Alexander Olzem <olzemal@pm.me>
2025-01-14 16:02:58 +0100

    Add more k8s tools

--- apps/k8s-tools.nix
+++ apps/k8s-tools.nix
@@ -4,11 +4,14 @@
   home.packages = with pkgs; [
     crossplane-cli
     docker-buildx
+    grafana-loki
     k9s
     kind
     kubectl
     kubernetes-helm
     minikube
+    talosctl
     trivy
+    zarf
   ];
 }
