35816213b018690e1bb9059e2d2832db50be131e
Alexander Olzem <olzemal@pm.me>
2026-01-09 15:42:45 +0100

    Fix tailscale version

--- hosts/features/tailscale.nix
+++ hosts/features/tailscale.nix
@@ -1,9 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs.unstable; [
-    tailscale
-  ];
-
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    package = pkgs.unstable.tailscale;
+  };
 }
