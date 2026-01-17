4832f933b6011964a17585ba08d6f9db3456d5e3
Alexander Olzem <alexander.olzem@bwi.de>
2026-01-17 09:53:49 +0100

    Simplify kitty.conf

--- homes/alex/features/terminals/kitty.nix
+++ homes/alex/features/terminals/kitty.nix
@@ -4,11 +4,6 @@
   programs.kitty = {
     enable = true;
 
-    extraConfig = ''
-      ${pkgs.lib.readFile ./kitty.conf}
-      ${pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-      macos_option_as_alt left
-      ''}
-    '';
+    extraConfig = pkgs.lib.readFile ./kitty.conf;
   };
 }
