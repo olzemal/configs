5e145a673ff214ea2a17a65d08b2dc23dc5b8218
Alexander Olzem <alexander.olzem@bwi.de>
2026-01-27 14:29:31 +0100

    Add flux cli

--- homes/features/kubernetes/client.nix
+++ homes/features/kubernetes/client.nix
@@ -4,14 +4,16 @@
   home.packages = with pkgs; [
     crossplane-cli
     docker-buildx
+    fluxcd
+    grafana-loki
     k9s
     kind
     kubectl
     kubernetes-helm
     minikube
-    grafana-loki
     talosctl
     trivy
+
     inputs.ocm.packages.${pkgs.system}.ocm
   ];
 }
