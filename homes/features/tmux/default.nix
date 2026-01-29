63e36b88dca60d3cf2914a200d0373eb5e9803b5
Alexander Olzem <alexander.olzem@bwi.de>
2026-01-29 11:59:02 +0100

    tweak tmux

--- homes/features/tmux/default.nix
+++ homes/features/tmux/default.nix
@@ -7,8 +7,18 @@
 
     plugins = with pkgs.tmuxPlugins; [
       sensible
-      gruvbox
       yank
+      {
+        plugin = power-theme;
+        extraConfig = ''
+          set -g @tmux_power_theme 'everforest'
+          set -g @tmux_power_right_arrow_icon    ''
+          set -g @tmux_power_left_arrow_icon     ''
+          set -g @tmux_power_show_user    false
+          set -g @tmux_power_show_host    false
+          set -g @tmux_power_show_session false
+        '';
+      }
     ];
   };
 }
