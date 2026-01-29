e61a8488876741572ef00416febe21d70e082cc9
Alexander Olzem <alexander.olzem@bwi.de>
2026-01-29 13:41:44 +0100

    Tweak tmux

--- homes/features/tmux/default.nix
+++ homes/features/tmux/default.nix
@@ -1,24 +1,34 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./tmux.conf;
-
-    plugins = with pkgs.tmuxPlugins; [
+    package = pkgs.unstable.tmux;
+    plugins = with pkgs.unstable.tmuxPlugins; [
       sensible
       yank
       {
-        plugin = power-theme;
+        plugin = minimal-tmux-status;
         extraConfig = ''
-          set -g @tmux_power_theme 'everforest'
-          set -g @tmux_power_right_arrow_icon    ''
-          set -g @tmux_power_left_arrow_icon     ''
-          set -g @tmux_power_show_user    false
-          set -g @tmux_power_show_host    false
-          set -g @tmux_power_show_session false
+          set -g status-interval 1
+
+          set -g @minimal-tmux-fg "colour0"
+          set -g @minimal-tmux-bg "colour12"
+          set -g @minimal-tmux-justify "left"
+          set -g @minimal-tmux-indicator false
+
+          set -g @minimal-tmux-status "top"
+          set -g @minimal-tmux-left false
+          set -g @minimal-tmux-right true
+          set -g @minimal-tmux-status-right "#[bg=colour0]#[fg=colour12]#[bg=colour12]#[fg=colour0]%d.%m.%Y %H:%M#[bg=colour0]#[fg=colour12]"
+
+          set -g @minimal-tmux-use-arrow true
+          set -g @minimal-tmux-right-arrow ""
+          set -g @minimal-tmux-left-arrow ""
+
         '';
       }
     ];
+    extraConfig = builtins.readFile ./tmux.conf;
   };
 }
