6bfbf4a383b14b21ee91c41783f17a2e2511a9f6
Alexander Olzem <olzemal@pm.me>
2026-03-14 15:05:06 +0100

    Dont manage firefox profile for now

--- home-manager-modules/features/browsers/default.nix
+++ home-manager-modules/features/browsers/default.nix
@@ -8,14 +8,7 @@
 
   config = {
     features.firefox.enable = lib.mkDefault config.features.desktopapps.enable;
-    programs.firefox = lib.mkIf config.features.firefox.enable {
-      enable = true;
-      profiles."default" = {
-        settings = {
-          "browser.compactmode.show" = true;
-        };
-      };
-    };
+    programs.firefox.enable = config.features.firefox.enable;
 
     programs.chromium.enable = config.features.chromium.enable;
   };
